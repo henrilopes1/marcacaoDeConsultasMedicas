a15421ca55185fc8e06edd188f754aeee8bbc47b
henrilopes <henrilopes1@gmail.com>
2025-04-03 09:25:39 -0300

    Aula 03-04

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,53 +1,53 @@
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import React from 'react';
-import { Button } from 'react-native-elements';
 import styled from 'styled-components/native';
+import { Button } from 'react-native-elements';
 import { HeaderContainer, HeaderTitle } from '../components/Header';
 import theme from '../styles/theme';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 type RootStackParamList = {
-    Home: undefined;
-    CreateAppointment: undefined;
-    Profile: undefined;
+  Home: undefined;
+  CreateAppointment: undefined;
+  Profile: undefined;
 };
 
 type ProfileScreenProps = {
-    navigation: NativeStackNavigationProp<RootStackParamList, 'Profile'>;
+  navigation: NativeStackNavigationProp<RootStackParamList, 'Profile'>;
 };
 
 const ProfileScreen: React.FC<ProfileScreenProps> = ({ navigation }) => {
-    return (
-        <Container>
-            <HeaderContainer>
-                <HeaderTitle>Meu Perfil</HeaderTitle>
-            </HeaderContainer>
+  return (
+    <Container>
+      <HeaderContainer>
+        <HeaderTitle>Meu Perfil</HeaderTitle>
+      </HeaderContainer>
 
-            <Content>
-                <Button
-                    title="Voltar"
-                    icon={{
-                        name: 'arrow-left',
-                        type: 'font-awesome',
-                        size: 20,
-                        color: 'white'
-                    }}
-                    buttonStyle={{
-                        backgroundColor: theme.colors.primary,
-                        borderRadius: 8,
-                        padding: 12,
-                        marginBottom: 20
-                    }}
-                    onPress={() => navigation.goBack()}
-                />
+      <Content>
+        <Button
+          title="Voltar"
+          icon={{
+            name: 'arrow-left',
+            type: 'font-awesome',
+            size: 20,
+            color: 'white'
+          }}
+          buttonStyle={{
+            backgroundColor: theme.colors.primary,
+            borderRadius: 8,
+            padding: 12,
+            marginBottom: 20
+          }}
+          onPress={() => navigation.goBack()}
+        />
 
-                <ProfileInfo>
-                    <Avatar source={{ uri: 'https://via.placeholder.com/150' }} />
-                    <Name>Nome do Usuário</Name>
-                    <Email>usuario@email.com</Email>
-                </ProfileInfo>
-            </Content>
-        </Container>
-    );
+        <ProfileInfo>
+          <Avatar source={{ uri: 'https://via.placeholder.com/150' }} />
+          <Name>Nome do Usuário</Name>
+          <Email>usuario@email.com</Email>
+        </ProfileInfo>
+      </Content>
+    </Container>
+  );
 };
 
 const Container = styled.View`
